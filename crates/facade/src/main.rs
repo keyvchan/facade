8bf59ba6557b06150df17b32db393b5d67519e82
Keyv Chan <keyvchan@gmail.com>
2022-11-26 21:15:15 +0800

    deps: config/serde/clap

--- crates/facade/src/main.rs
+++ crates/facade/src/main.rs
@@ -1,3 +1,5 @@
+mod config;
+
 use log::debug;
 
 #[tokio::main]
@@ -5,6 +7,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     debug!("Logging works!");
+
+    // load config
 
     let mut socks_server = socks::SocksServer::new("127.0.0.1:1080").await?;
     socks_server.serve().await?;
