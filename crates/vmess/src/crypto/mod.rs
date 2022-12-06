35ff0ce1198f236e0b4e78171868577b575f3b93
Keyv Chan <keyvchan@gmail.com>
2022-12-06 18:16:23 +0800

    docs: add description of kdf implementation

--- crates/vmess/src/crypto/mod.rs
+++ crates/vmess/src/crypto/mod.rs
@@ -2,19 +2,36 @@ use dyn_clone::DynClone;
 use sha2::{Digest, Sha256};
 use std::iter::Iterator;
 
+/// Hash is a trait which emulates the hash.Hash interface in Go.
 trait Hash: DynClone {
+    /// write adds more data to the running hash.
     fn write(&mut self, data: Vec<u8>);
+
+    /// sum appends the current hash to b and returns the resulting slice.
+    /// This does not change the underlying hash state.
     fn sum(&mut self, i: Option<Vec<u8>>) -> Vec<u8>;
+
+    /// reset resets the Hash to its initial state.
     fn reset(&mut self);
+
+    /// size returns the number of bytes Sum will return.
     fn size(&self) -> usize;
+
+    /// block_size returns the hash's underlying block size.
     fn block_size(&self) -> usize;
 }
 
+/// Hmac is a struct which emulates the hmac.Hmac struct in Go.
+/// marshalable option is not implemented.
 #[derive(Clone)]
 struct Hmac {
+    /// opad is the outer padding.
     opad: Vec<u8>,
+    /// ipad is the inner padding.
     ipad: Vec<u8>,
+    /// inner is the inner hash.
     inner: Box<dyn Hash>,
+    /// outer is the outer hash.
     outer: Box<dyn Hash>,
 }
 
@@ -24,7 +41,7 @@ impl Hash for Hmac {
     }
 
     fn sum(&mut self, input: Option<Vec<u8>>) -> Vec<u8> {
-        // we shoul check if i is none
+        // TODO: we shoul check if i is none
         let (orig_len, input) = match input {
             Some(i) => (i.len(), i),
             None => (0, vec![].to_vec()),
@@ -56,10 +73,12 @@ impl Hash for Hmac {
 dyn_clone::clone_trait_object!(Hash);
 
 impl Hmac {
+    // emulates the hmac.New function in Go.
     fn new_hasher(hasher: Box<dyn Hash>, mut key: Vec<u8>) -> Box<dyn Hash> {
         // new hash
+        // FIXME: memory allocation should be optimized.
         let mut outer = hasher.clone();
-        let mut inner = hasher.clone();
+        let mut inner = hasher;
 
         let block_size = inner.block_size();
         let mut ipad = vec![0; block_size];
@@ -89,6 +108,7 @@ impl Hmac {
     }
 }
 
+/// Sha256Hasher wraps the sha256 hasher in sha2. It implements the Hash trait.
 #[derive(Clone)]
 struct Sha256Hasher {
     hasher: Sha256,
@@ -128,21 +148,21 @@ impl Sha256Hasher {
     }
 }
 
-fn sha256() -> Box<dyn Hash> {
-    Box::new(Sha256Hasher::new())
-}
-
+/// calculate the kdf for given key and path
 pub(crate) fn kdf(key: &[u8], path: Vec<Vec<u8>>) -> Vec<u8> {
-    let mut mac1 = Hmac::new_hasher(sha256(), "VMess AEAD KDF".as_bytes().to_vec());
-
-    // feed the calculated mac1 to a new hasher, in reverse order
+    // TODO: hard coded token shoule be in a seperate file
+    let mut mac = Hmac::new_hasher(
+        Box::new(Sha256Hasher::new()),
+        "VMess AEAD KDF".as_bytes().to_vec(),
+    );
 
     for x in path.iter().rev() {
-        let mac2 = Hmac::new_hasher(mac1, x.to_vec());
-        mac1 = mac2;
+        // feed the calculated mac1 to a new hasher, in reverse order
+        let new_mac = Hmac::new_hasher(mac, x.to_vec());
+        mac = new_mac;
     }
 
-    mac1.write(key.to_vec());
+    mac.write(key.to_vec());
 
-    mac1.sum(None)
+    mac.sum(None)
 }
